b1cb529f98bb0b5b18ae638667cac516744e1b93
Tom Oliveira <washington-kd@hotmail.com>
2022-02-16 13:49:00 -0300

    adicionando teste unitario no projeto

---
+++ tests/UploadTest.php
@@ -0,0 +1,32 @@
+<?php
+require "vendor/autoload.php";
+
+use App\CommandLine;
+use App\DotenvLoad;
+use App\FileUploader;
+use Dotenv\Dotenv;
+
+test('testarUploadDeArquivo', function () {
+    $currentPath = realpath(__DIR__."/../");
+    
+    $arr = [
+        "./run",
+        "--filepath={$currentPath}/composer.json",
+        "--folder=test"
+    ];
+
+    $dotenv = Dotenv::createImmutable($currentPath);
+
+    $cli = new CommandLine($arr);
+    $envLoad = new DotenvLoad($dotenv);
+
+    $cli->start();
+
+    $fileUploader = new FileUploader($envLoad, $cli, $currentPath);
+
+    $infos = $fileUploader->upload();
+
+    echo json_encode($infos);
+
+    expect(true)->toBeTrue();
+});
