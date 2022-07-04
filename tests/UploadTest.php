9fb88002224e6a08b8d5895fb099c37c83e23288
Tom Oliveira Bastos <washington-kd@hotmail.com>
2022-07-04 13:27:39 -0300

    att test unitario

--- tests/UploadTest.php
+++ tests/UploadTest.php
@@ -1,17 +1,45 @@
 <?php
 
-require "vendor/autoload.php";
+require __DIR__ . "/../vendor/autoload.php";
 
 use App\Service\CommandLine;
 use App\Service\DotenvLoad;
 use App\Service\FileUploader;
 use Dotenv\Dotenv;
+use App\Service\OauthGenerator;
 
-test('testarUploadDeArquivo', function () {
-    $currentPath = realpath(__DIR__."/../");
+$currentPath = realpath(__DIR__ . "/../");
+
+test('testGenerateAccessToken', function () use ($currentPath) {
+    $dotenv = Dotenv::createImmutable($currentPath);
+
+    $dotenv->load();
+
+    $oauth = new OauthGenerator();
+
+    $refreshTokenPath = $currentPath . "/refresh_token.txt";
+
+    $accessTokenFileObject = $currentPath . "/access_token.txt";
+
+    $refreshToken = trim(file_get_contents($refreshTokenPath));
+
+    $accessTokenObjectJson = $oauth->getToken($refreshToken);
+
+    $accessTokenObject = json_decode($accessTokenObjectJson, true);
+
+    $datetime = new \DateTime('now');
+
+    $accessTokenObject['datetime'] = $datetime->format(DateTime::ATOM);
+
+    file_put_contents($accessTokenFileObject, json_encode($accessTokenObject));
+
+    expect(true)->toBeTrue();
+});
+
+test('testUploadDeArquivo', function () use ($currentPath) {
 
     $arr = [
-        "./run",
+        "./run-v2",
         "--filepath={$currentPath}/composer.json",
         "--folder=test",
         "--uniqueid=asdahsduasudha",
@@ -23,6 +51,10 @@ test('testarUploadDeArquivo', function () {
     $cli = new CommandLine($arr);
 
     $envLoad = new DotenvLoad($dotenv);
+
+    $json = trim(file_get_contents("{$currentPath}/access_token.txt"));
+
+    $_ENV['ACCESS_TOKEN'] = json_decode($json, true)['access_token'];
 
     $cli->start();
 
